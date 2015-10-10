b37cd6f98d40baea3c27447f4ed019c28333237d
Kevin Chung <kvnchung92@gmail.com>
2015-10-10 03:57:49 -0500

    small edit to login auth to get ready for parse integration

--- dist/app/controllers/auth.js
+++ dist/app/controllers/auth.js
@@ -1,19 +1,30 @@
-
+		
 
 var app = angular.module('controller.auth', []);
 
 app.controller('LoginController', function($scope) {
 	
+	var myEl = angular.element( document.querySelector( '#passwordField' ) );
+
 	
-	console.log('hi');
 	$scope.user={
 		email:'',
 		password:''
 	}
 
+
+
 	$scope.login = function(){
 		console.log($scope.user.email);
 		console.log($scope.user.password);
+	}
+	$scope.check = function(){
+		console.log($scope.user.password.length);
+		console.log($scope.user.email);
+
+		if($scope.user.email && $scope.user.password.length>0){
+			//validation code here
+		}
 	}
 
 });
