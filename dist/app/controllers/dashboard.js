9de077eb8f6eadcbc0b9efaf724ffdd809132e60
Sujan Shrestha <sujan.stha000@gmail.com>
2015-10-10 02:47:18 -0500

    added mock data for breaks

--- dist/app/controllers/dashboard.js
+++ dist/app/controllers/dashboard.js
@@ -13,6 +13,29 @@
 	  $scope.onClick = function (points, evt) {
 	    console.log(points, evt);
 	  };
+
+	  $scope.breaks = {
+	  	{
+	  		time: '10 min',
+	  		title: 'Stand up and stretch',
+	  		description: 'This is a test description for stand up and stretch.'
+	  	},
+	  	{
+	  		time: '1 hour',
+	  		title: 'Take a nap',
+	  		description: 'This is a test description for take a nap.'
+	  	},
+	  	{
+	  		time: '2 hours 20 min',
+	  		title: 'Stand up and stretch',
+	  		description: 'This is a test description for stand up and stretch.'
+	  	},
+	  	{
+	  		time: '3 hours',
+	  		title: 'Stand up and stretch',
+	  		description: 'This is a test description for stand up and stretch.'
+	  	}
+	  }
 	});
 
 })();
