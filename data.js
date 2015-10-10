43664b775da86a1d55edf95d01661b2b82f59536
Nhat Dao <daoanhnhat1995@gmail.com>
2015-10-10 03:07:23 -0500

    all ctrl+ keys added

--- data.js
+++ data.js
@@ -2,11 +2,19 @@
 var _ = require('underscore');
 var globalShortcut = require('global-shortcut');
 var data = require('./models/shortcuts');
+
 exports.shortCuts = function(){
 
 	// map input shortcut with list data
 	_.map(data.get(), function(key) {
-		var ret = globalShortcut.register(key, function() {console.log(key);});
+		var ret = globalShortcut.register(key, function() {
+			console.log(key);
+
+			//free the key
+			globalShortcut.unregister(key);
+
+		});
+
     
     if (!ret) {  console.log("registration failed");}
 
