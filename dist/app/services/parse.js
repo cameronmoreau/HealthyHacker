5b3e5b7ecb2500411b2bb9bb0766840a91367b0e
Nhat Dao <daoanhnhat1995@gmail.com>
2015-10-10 13:23:40 -0500

    switch task

--- dist/app/services/parse.js
+++ dist/app/services/parse.js
@@ -16,43 +16,6 @@
     });
 
 
-// REST API from Parse Events
-app.factory ('Mouse', ['$http','PARSE_CREDENTIALS',function($http,PARSE_CREDENTIALS){
-  
-  return{
-    getAll: function(){
-      return $http.get('https://api.parse.com/1/classes/Mouse',{
-                headers:{
-                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
-                }
-            });
-
-    },
-    create: function(data){
-       return $http.post('https://api.parse.com/1/classes/Mouse',data,{
-                headers:{
-                    'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-                    'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
-                    'Content-Type':'application/json'
-                }
-            });
-    }
-    // edit:function(id,data){
-    //         return $http.put('https://api.parse.com/1/classes/Mouse/'+id,data,{
-    //             headers:{
-    //                 'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-    //                 'X-Parse-REST-API-Key':PARSE_CREDENTIALS.REST_API_KEY,
-    //                 'Content-Type':'application/json'
-    //             }
-    //         });
-    //     }
-  }
-}]);
-
-
-
-
 
 
 
